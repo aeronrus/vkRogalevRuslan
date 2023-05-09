c8a5ba2cdf7dbe3e9ad661b067dac72bda732727
Ruslan Rogalev <rre@m-ppo.ru>
2023-05-09 13:46:21 +0300

    some chandes in frontend side

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -7,7 +7,7 @@ export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('user')) || null);
 
   const login = async (inputs) => {
-    const res = await axios.post('http://localhost:8800/api/auth/login', inputs, {
+    const res = await axios.post('http://localhost:7777/api/auth/login', inputs, {
       withCredentials: true,
     });
 
--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post('http://localhost:8800/api/auth/register', inputs);
+      await axios.post('http://localhost:7777/api/auth/register', inputs);
     } catch (err) {
       setErr(err.response.data);
     }
